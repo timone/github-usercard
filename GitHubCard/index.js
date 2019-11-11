ea4552a24b4c7ea1cbcad3704ad1152e25e164cb
Timur Mone <timur@yurtah.com>
2019-11-11 17:14:47 -0500

    read.me and Index.html script

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,5 +1,5 @@
 /* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
+           (replacing the placeholder with your Github name):
            https://api.github.com/users/<your name>
 */
 
